1f0c626f820e0147651482b36a00ccfc91e91b60
James Morgan <james.morgan.e@googlemail.com>
2018-03-03 22:31:44 +0000

    Adding basic count of total purchased/available

--- src/components/PurchaseEdition.vue
+++ src/components/PurchaseEdition.vue
@@ -2,7 +2,15 @@
   <div class="assets_to_buy">
     <h1>{{ title }}</h1>
 
-    {{assetsForArtistAndEditionByType($route.params.artist, $route.params.edition)}}
+    <div v-for="value, key in assetsForArtistAndEditionByType($route.params.artist, $route.params.edition)">
+      <p>Purchase the <strong>{{key}}</strong> asset</p>
+      <p>Available {{ countAvailable(value).length }} | Purchased {{ countPurchased(value).length }}</p>
+
+      <edition :edition="value[0]" :count="value.length"></edition>
+      
+      {{ value }}
+
+    </div>
 
   </div>
 </template>
@@ -10,10 +18,12 @@
 <script>
   import {mapGetters, mapState} from 'vuex'
   import Artist from './Artist'
+  import Edition from './Edition'
+  import _ from 'lodash'
 
   export default {
     name: 'purchaseEdition',
-    components: {},
+    components: {Edition},
     data() {
       return {
         title: 'Assets to buy',
@@ -21,13 +31,21 @@
     },
     computed: {
       ...mapGetters([
-        'assetsForArtistAndEdition',
         'assetsForArtistAndEditionByType'
-      ])
+      ]),
+
     },
     mounted() {
 
     },
+    methods: {
+      countPurchased: (assets) => {
+        return _.filter(assets, {'purchased': 1})
+      },
+      countAvailable: (assets) => {
+        return _.filter(assets, {'purchased': 0})
+      }
+    }
   }
 </script>
 
