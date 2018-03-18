b91862b53ea1adbd99818d8c9f3402c9ad2e9280
James Morgan <james.morgan.e@googlemail.com>
2018-03-18 20:56:32 +0000

    Adding artists bios

--- src/store/artist-data.js
+++ src/store/artist-data.js
@@ -4,7 +4,7 @@ const artists = [
     name: 'Barrie J Davis',
     twitter: '@barriejdavies',
     bio: 'Hello, I\'m Barrie J Davies and I\'m an artist (born 1977) I\'m a British artist from Milford Haven, Pembrokeshire, Wales. I graduated from the Southampton Institute with a Fine Art degree in 2000 and I completed my Master\'s Degree at the University of Wales Institute, Cardiff in 2004. I now live and work in Brighton, England UK. My artwork is a fun colourful psychedelic and humourous approach to expose the human condition: notions of success, money, glamour, love, death, sex, gender & religion are picked at with dry comedic use of tragedy meshed with absurdity.',
-    img: 'https://static1.squarespace.com/static/5a8b2af5692ebefdc3fc957a/t/5a948d3a652dea86531e1b20/1519684938013/BarrieJD.png',
+    img: 'https://photos-1.dropbox.com/t/2/AADIZbTqysC5c7p2ZyOj-3-pE8Dmpfe3eIFuJzwPHZcsBA/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistBarrieD.jpg/EIiCtDkY2-sBIAIoAg/QYuUrSUNwJCEnE7FoBPBSU0KSTVoN7GaJdZtVe1Tz4o?dl=0&size=2048x1536&size_mode=3',
     featured: true
   },
   {
@@ -12,7 +12,7 @@ const artists = [
     name: 'James O’Connell',
     twitter: '@Jamesp0p',
     bio: 'James Oconnell is a Manchester based creative who has a passion for mixing colour and lines. He applies his minimalistic style to a variety of themes and has created work for the likes of Wired Magazine, Youtube, T3 Magazine, BBC and The Atlantic to name a few. He is hugely inspired by contemporary pop culture in all its forms - from motion pictures, music to advertising and sport. Simplicity is the key to his work allowing the expression of the idea to reign supreme.',
-    img: 'https://static1.squarespace.com/static/5a8b2af5692ebefdc3fc957a/t/5a948a6de4966b8b93cd2a3b/1519684215997/JaneB.png',
+    img: 'https://photos-3.dropbox.com/t/2/AADS_7287PcxXgF_e-Gvc6Y4-xOVwAspLp0HXgF2d9UErw/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistJamesO.jpg/EIiCtDkY2-sBIAIoAg/EYXw-Oz20ExYmPyf3oD9bg-C3_8EneJ9C3faregnop0?dl=0&size=2048x1536&size_mode=3',
     featured: true
   },
   {
@@ -20,7 +20,7 @@ const artists = [
     name: 'Matt Ferres',
     twitter: '@saveferres',
     bio: '...TODO...',
-    img: 'https://static1.squarespace.com/static/5a8b2af5692ebefdc3fc957a/t/5a94896c0d929738ef116393/1519683968371/JamesO_profile.png',
+    img: '',
     featured: true
   },
   {
@@ -28,35 +28,50 @@ const artists = [
     name: 'Peter Davis',
     twitter: '@peterdavis_art',
     bio: 'I am a prize-winning professional portrait painter and elected member of the Manchester Academy of Fine Arts (MAFA). My aim, as a social realist painter, is to capture the spirit of the age and create contemporary portraiture that tells stories about my sitters through a strong visual narrative.',
-    img: 'https://static1.squarespace.com/static/5a8b2af5692ebefdc3fc957a/t/5a94896c0d929738ef116393/1519683968371/JamesO_profile.png'
+    img: 'https://photos-1.dropbox.com/t/2/AABbUFknurNHABFjljxbIgHGRIXARo0NQkVYeAllmf5jtQ/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistPeterD.jpg/EIiCtDkY2usBIAIoAg/tXDkTLNh5lSEqHOl1SP1Ps6hDtxpCUG16zz_Lsh2l8k?dl=0&size=2048x1536&size_mode=3' +
+    ''
   },
   {
     id: 'STA',
     name: 'Sam Taylor',
     twitter: '@SptSam',
     bio: 'Sam Taylor is known for his bold, unbridled and explosive artworks. Outrageous colour schemes and intricate line-work are partnered up with characters and motifs to create his fun, mad world.Originally from Leicester, he\'s worked with a wealth of impressive clients since moving to London many moons ago – including Google, The New Yorker, Vans and Nickelodeon.',
-    img: 'https://static1.squarespace.com/static/5a8b2af5692ebefdc3fc957a/t/5a94896c0d929738ef116393/1519683968371/JamesO_profile.png'
+    img: 'https://photos-2.dropbox.com/t/2/AAB1AsZWfVnJBU7RMMVOXK3cL4Rw8hNordayP_nhCAXpjQ/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistSamT.jpg/EIiCtDkY2usBIAIoAg/h0g7xRXlX7P7H41KpzJhvrPOYOZApA2BdCsEcku6qtc?dl=0&size=2048x1536&size_mode=3'
   },
   {
     id: 'JBO',
     name: 'Jane Bowyer',
     twitter: '@bowyerjane',
     bio: 'Jane Bowyer is an independent graphic designer and illustrator based in Manchester.',
-    img: 'https://static1.squarespace.com/static/5a8b2af5692ebefdc3fc957a/t/5a94896c0d929738ef116393/1519683968371/JamesO_profile.png'
+    img: 'https://photos-3.dropbox.com/t/2/AAB-NQuHZsHuFDDjyhUYWSdLpgeZAjnHbVfcmcG1-0Bopw/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistJaneB.jpg/EIiCtDkY2-sBIAIoAg/SXlnbR0VZPyk5Xz5E52iUrV3PupTCwAdU2bNaPqgzTQ?dl=0&size=2048x1536&size_mode=3'
+  },
+  {
+    id: 'CHE',
+    name: 'Chris English',
+    twitter: '@afullenglish',
+    bio: '...TODO...',
+    img: 'https://photos-2.dropbox.com/t/2/AAA4WjqdedbkYEHu76Vr4HN6HejzoBsHRAgjzWB5Svw1rQ/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistChrisE.jpg/EIiCtDkY2esBIAIoAg/A81of4fgRwskcWkcZQniuyfnbxx1ZWXqNqiz8hOYPmk?dl=0&size=2048x1536&size_mode=3'
   },
   {
     id: 'DAR',
     name: 'David Arnott',
     twitter: '@DavidPArnott7',
     bio: 'David Arnott is a Mosaic Artist based in Salford Manchester. He take popular and cultural icons and creates an image from hand cut pieces of ceramic tile',
-    img: 'https://static1.squarespace.com/static/5a8b2af5692ebefdc3fc957a/t/5a94896c0d929738ef116393/1519683968371/JamesO_profile.png'
+    img: ''
   },
   {
     id: 'KOR',
     name: 'Katie O\'Rourke',
     twitter: '@katieor_design',
     bio: 'I\'m a graphic designer and illustrator, big on travelling and all things creative.',
-    img: 'https://static1.squarespace.com/static/5a8b2af5692ebefdc3fc957a/t/5a94896c0d929738ef116393/1519683968371/JamesO_profile.png'
+    img: ''
+  },
+  {
+    id: 'LHK',
+    name: 'Laura Hawkins AKA Paper Hawk',
+    twitter: '@paper_hawk',
+    bio: '...TODO...',
+    img: ''
   }
 ];
 
