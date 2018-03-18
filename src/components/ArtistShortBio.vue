72dda6cd4a39d3a5c54eccb9e5860d13505fa759
Andy Gray <andy@whatsthescore.co.uk>
2018-03-18 13:18:57 +0000

    short bio component

---
+++ src/components/ArtistShortBio.vue
@@ -0,0 +1,21 @@
+<template>
+  <div v-if="artist">
+      <img :src="artist.img" style="max-width: 150px; float:right"/>
+      <h1>{{ artist.name }}</h1>
+
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'artistShortBio',
+    props: ['artist'],
+  };
+</script>
+
+<style scoped>
+  .twitter {
+    background-color: deepskyblue;
+    padding: 10px;
+  }
+</style>
