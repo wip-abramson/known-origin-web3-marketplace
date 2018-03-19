81fb00b3556bd71fca70f2a5036ee202260631e9
James Morgan <james.morgan.e@googlemail.com>
2018-03-19 09:18:59 +0000

    Adding images

--- src/store/artist-data.js
+++ src/store/artist-data.js
@@ -4,7 +4,7 @@ const artists = [
     name: 'Barrie J Davis',
     twitter: '@barriejdavies',
     bio: 'Hello, I\'m Barrie J Davies and I\'m an artist (born 1977) I\'m a British artist from Milford Haven, Pembrokeshire, Wales. I graduated from the Southampton Institute with a Fine Art degree in 2000 and I completed my Master\'s Degree at the University of Wales Institute, Cardiff in 2004. I now live and work in Brighton, England UK. My artwork is a fun colourful psychedelic and humourous approach to expose the human condition: notions of success, money, glamour, love, death, sex, gender & religion are picked at with dry comedic use of tragedy meshed with absurdity.',
-    img: 'https://photos-1.dropbox.com/t/2/AADIZbTqysC5c7p2ZyOj-3-pE8Dmpfe3eIFuJzwPHZcsBA/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistBarrieD.jpg/EIiCtDkY2-sBIAIoAg/QYuUrSUNwJCEnE7FoBPBSU0KSTVoN7GaJdZtVe1Tz4o?dl=0&size=2048x1536&size_mode=3',
+    img: 'assets/artists/ArtistBarrieD_preview.jpeg',
     featured: true
   },
   {
@@ -12,7 +12,7 @@ const artists = [
     name: 'James O’Connell',
     twitter: '@Jamesp0p',
     bio: 'James Oconnell is a Manchester based creative who has a passion for mixing colour and lines. He applies his minimalistic style to a variety of themes and has created work for the likes of Wired Magazine, Youtube, T3 Magazine, BBC and The Atlantic to name a few. He is hugely inspired by contemporary pop culture in all its forms - from motion pictures, music to advertising and sport. Simplicity is the key to his work allowing the expression of the idea to reign supreme.',
-    img: 'https://photos-3.dropbox.com/t/2/AADS_7287PcxXgF_e-Gvc6Y4-xOVwAspLp0HXgF2d9UErw/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistJamesO.jpg/EIiCtDkY2-sBIAIoAg/EYXw-Oz20ExYmPyf3oD9bg-C3_8EneJ9C3faregnop0?dl=0&size=2048x1536&size_mode=3',
+    img: 'artists/ArtistJamesO_preview.jpeg',
     featured: true
   },
   {
@@ -28,29 +28,28 @@ const artists = [
     name: 'Peter Davis',
     twitter: '@peterdavis_art',
     bio: 'I am a prize-winning professional portrait painter and elected member of the Manchester Academy of Fine Arts (MAFA). My aim, as a social realist painter, is to capture the spirit of the age and create contemporary portraiture that tells stories about my sitters through a strong visual narrative.',
-    img: 'https://photos-1.dropbox.com/t/2/AABbUFknurNHABFjljxbIgHGRIXARo0NQkVYeAllmf5jtQ/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistPeterD.jpg/EIiCtDkY2usBIAIoAg/tXDkTLNh5lSEqHOl1SP1Ps6hDtxpCUG16zz_Lsh2l8k?dl=0&size=2048x1536&size_mode=3' +
-    ''
+    img: 'artists/ArtistPeterD_preview.jpeg'
   },
   {
     id: 'STA',
     name: 'Sam Taylor',
     twitter: '@SptSam',
     bio: 'Sam Taylor is known for his bold, unbridled and explosive artworks. Outrageous colour schemes and intricate line-work are partnered up with characters and motifs to create his fun, mad world.Originally from Leicester, he\'s worked with a wealth of impressive clients since moving to London many moons ago – including Google, The New Yorker, Vans and Nickelodeon.',
-    img: 'https://photos-2.dropbox.com/t/2/AAB1AsZWfVnJBU7RMMVOXK3cL4Rw8hNordayP_nhCAXpjQ/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistSamT.jpg/EIiCtDkY2usBIAIoAg/h0g7xRXlX7P7H41KpzJhvrPOYOZApA2BdCsEcku6qtc?dl=0&size=2048x1536&size_mode=3'
+    img: '/assets//artists/ArtistSamT_preview.jpeg'
   },
   {
     id: 'JBO',
     name: 'Jane Bowyer',
     twitter: '@bowyerjane',
     bio: 'Jane Bowyer is an independent graphic designer and illustrator based in Manchester.',
-    img: 'https://photos-3.dropbox.com/t/2/AAB-NQuHZsHuFDDjyhUYWSdLpgeZAjnHbVfcmcG1-0Bopw/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistJaneB.jpg/EIiCtDkY2-sBIAIoAg/SXlnbR0VZPyk5Xz5E52iUrV3PupTCwAdU2bNaPqgzTQ?dl=0&size=2048x1536&size_mode=3'
+    img: '/assets//artists/ArtistJaneB_preview.jpeg'
   },
   {
     id: 'CHE',
     name: 'Chris English',
     twitter: '@afullenglish',
     bio: '...TODO...',
-    img: 'https://photos-2.dropbox.com/t/2/AAA4WjqdedbkYEHu76Vr4HN6HejzoBsHRAgjzWB5Svw1rQ/12/73985167/jpeg/32x32/3/1521421200/0/2/ArtistChrisE.jpg/EIiCtDkY2esBIAIoAg/A81of4fgRwskcWkcZQniuyfnbxx1ZWXqNqiz8hOYPmk?dl=0&size=2048x1536&size_mode=3'
+    img: '/assets//artists/ArtistChrisE_preview.jpeg'
   },
   {
     id: 'DAR',
