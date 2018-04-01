cfc0a419910a73c84be7c6bb058278fac6fd0fa5
James Morgan <james.morgan.e@googlemail.com>
2018-04-01 16:24:23 +0100

    Adding straplines and fixing buzz

--- src/store/artist-data.js
+++ src/store/artist-data.js
@@ -3,7 +3,7 @@ const artists = [
     artistCode: 'BJD',
     name: 'Barrie J Davis',
     twitter: '@barriejdavies',
-    strapline: 'Hello, I\'m Barrie J Davies',
+    strapline: 'Hello, I\'m Barrie J Davies and I\'m an artist. My artwork is a fun colourful psychedelic and humourous approach to expose the human condition.',
     bio: 'Hello, I\'m Barrie J Davies and I\'m an artist (born 1977) I\'m a British artist from Milford Haven, Pembrokeshire, Wales. I graduated from the Southampton Institute with a Fine Art degree in 2000 and I completed my Master\'s Degree at the University of Wales Institute, Cardiff in 2004. I now live and work in Brighton, England UK. My artwork is a fun colourful psychedelic and humourous approach to expose the human condition: notions of success, money, glamour, love, death, sex, gender & religion are picked at with dry comedic use of tragedy meshed with absurdity.',
     img: 'static/artists/BarrieD_@x2.png',
     featured: true
@@ -12,8 +12,8 @@ const artists = [
     artistCode: 'JOC',
     name: 'James O\'Connell',
     twitter: '@Jamesp0p',
-    strapline: 'Manchester based creative and illustrator',
-    bio: 'James O\'Connell is a Manchester based creative who has a passion for mixing colour and lines. He applies his minimalistic style to a variety of themes and has created work for the likes of Wired Magazine, Youtube, T3 Magazine, BBC and The Atlantic to name a few. He is hugely inspired by contemporary pop culture in all its forms - from motion pictures, music to advertising and sport. Simplicity is the key to his work allowing the expression of the idea to reign supreme. ',
+    strapline: 'James O\'Connell is a Manchester based creative and illustrator. Simplicity is the key to his work allowing the expression of the idea to reign supreme.',
+    bio: 'James O\'Connell is a Manchester based creative who has a passion for mixing colour and lines. He applies his minimalistic style to a variety of themes and has created work for the likes of Wired Magazine, Youtube, T3 Magazine, BBC and The Atlantic to name a few. He is hugely inspired by contemporary pop culture in all its forms - from motion pictures, music to advertising and sport. Simplicity is the key to his work allowing the expression of the idea to reign supreme.',
     img: 'static/artists/JamesOc_@x2.png',
     featured: true
   },
@@ -21,7 +21,7 @@ const artists = [
     artistCode: 'PDA',
     name: 'Peter Davis',
     twitter: '@peterdavis_art',
-    strapline: 'Prize-winning professional portrait painter and member of MAFA',
+    strapline: 'I am a Prize-winning professional portrait painter and member of MAFA. My aim, as a social realist painter, is to capture the spirit of the age and create contemporary portraiture.',
     bio: 'I am a prize-winning professional portrait painter and elected member of the Manchester Academy of Fine Arts (MAFA). My aim, as a social realist painter, is to capture the spirit of the age and create contemporary portraiture that tells stories about my sitters through a strong visual narrative.',
     img: 'static/artists/PeterD_@x2.png',
     featured: false
@@ -30,9 +30,8 @@ const artists = [
     artistCode: 'STA',
     name: 'Sam Taylor',
     twitter: '@SptSam',
-    strapline: 'Independent graphic designer and illustrator.',
-    bio: 'Sam Taylor is known for his bold, unbridled and explosive artworks. Outrageous colour schemes and intricate line-work are partnered up with characters and motifs to create his fun, mad world.\n' +
-    'Originally from Leicester, he\'s worked with a wealth of impressive clients since moving to London many moons ago – including Google, The New Yorker, Vans and Nickelodeon.',
+    strapline: 'Sam Taylor is an independent graphic designer and illustrator known for his bold, unbridled and explosive artworks. Outrageous colour schemes and intricate line-work.',
+    bio: 'Sam Taylor is known for his bold, unbridled and explosive artworks. Outrageous colour schemes and intricate line-work are partnered up with characters and motifs to create his fun, mad world.Originally from Leicester, he\'s worked with a wealth of impressive clients since moving to London many moons ago – including Google, The New Yorker, Vans and Nickelodeon.',
     img: 'static/artists/SamT_@x2.png',
     featured: true
   },
@@ -40,6 +39,7 @@ const artists = [
     artistCode: 'JBO',
     name: 'Jane Bowyer',
     twitter: '@bowyerjane',
+    strapline: 'Jane Bowyer is an independent graphic designer and illustrator based in Manchester.',
     bio: 'Jane Bowyer is an independent graphic designer and illustrator based in Manchester.',
     img: 'static/artists/JaneB_@x2.png'
   },
@@ -62,6 +62,7 @@ const artists = [
     name: 'Katie O\'Rourke',
     twitter: '@katieor_design',
     bio: 'Katie is a graphic designer, illustrator and artist currently based in Manchester. Her love for travel influences her work, often exploring places she has visited through both hand drawn and digital media. She’s interested in the dichotomy between these two approaches and the different sense of connection to place that comes through in each.',
+    strapline: 'Katie is a graphic designer, illustrator and artist currently based in Manchester. Her love for travel influences her work, often exploring places she has visited.',
     img: 'static/artists/KatieORouke_@x2.png'
   },
   {
@@ -69,6 +70,7 @@ const artists = [
     name: 'Laura Hawkins AKA Paper Hawk',
     twitter: '@paper_hawk',
     bio: 'Laura Hawkins is a designer and illustrator based in Manchester. Her illustration alias ‘Paper Hawk’ was born of her desire to create illustrations full of character, charm and life - that weren\'t \'perfect\', but who\'s imperfections were what made them interesting.Working in graphic design since graduating from Falmouth College of Arts back in 2005 Laura had been increasingly drawn to illustration. After a recent move from London to Manchester, she began experimenting with using paper as the \'paint\', and discovered a passion she never knew existed.Her aim with Paper Hawk is to make engaging, interesting and unique illustrations that bring a brief to life. Using papers salvaged from magazines, newspapers, recycling bins, empty paper packaging, and donations from friends and colleagues her work aims to giving new life to something that would otherwise be discarded. She also creates some of the papers herself by painting with watercolours or inks and then tearing these up too.',
+    strapline: 'Laura Hawkins is a Illustrator & paper collage artist inspired by nature.',
     img: 'static/artists/LauraH_@x2.png'
   },
   {
@@ -76,6 +78,7 @@ const artists = [
     name: 'David Arnott',
     twitter: '@DavidPArnott7',
     bio: 'David Arnott is a Mosaic Artist based in Salford Manchester. He take popular and cultural icons and creates an image from hand cut pieces of ceramic tile',
+    strapline: 'David Arnott is a Mosaic Artist based in Salford Manchester. He take popular and cultural icon',
     img: 'static/artists/david-arnott_x2.png'
   },
   {
