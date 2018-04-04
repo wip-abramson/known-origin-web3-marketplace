bd6f97c04ddde36d3cdef56456fd893ccd3b8e97
James Morgan <james.morgan.e@googlemail.com>
2018-04-04 20:56:22 +0100

    Updated truffle flattern

--- truffle_flatten.sh
+++ truffle_flatten.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env bash
 
 node ./node_modules/.bin/truffle-flattener ./contracts/KnownOriginDigitalAsset.sol > ./contracts-flat/FLAT-KnownOriginDigitalAsset.sol;
+
+node ./node_modules/.bin/truffle-flattener ./contracts/Migrations.sol > ./contracts-flat/FLAT-Migrations.sol;
